100823ddd503ccaab2ae3d929969f2a4259bd749
Antonio Carlos <55151078+acsmcarlos@users.noreply.github.com>
2023-02-24 15:02:00 -0300

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,12 +1,14 @@
 const cypress = require('cypress')
 const tesults = require('cypress-tesults-reporter');
 
+const TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImExNDY5ZTlhLTMzODktNGUxZi1iZTA5LWZmNDY2YzNiODM1Ni0xNjc3MjYwMjg2MDY5IiwiZXhwIjo0MTAyNDQ0ODAwMDAwLCJ2ZXIiOiIwIiwic2VzIjoiYWRlZjA0NzktOThkNC00MzhkLThhMDMtNzIwNzdhZjY1ZDU3IiwidHlwZSI6InQifQ.dctBsQNxUMc0IEVrtzwA8RDFrvEUkncchQn4aT0hZzI'
+
 cypress.run({
   // specs to run here
 })
 .then((results) => {
   const args = {
-    target: ${{ TOKEN }},
+    target: TOKEN,
   }
   tesults.results(results, args);
 })
